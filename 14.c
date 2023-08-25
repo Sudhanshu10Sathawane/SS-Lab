47fcfc8fadfd5780307980993e790d420d153550
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-08-25 10:21:17 +0530

    25 Aug

---
+++ 14.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<sys/stat.h>
+#include<stdlib.h>
+int main(int argc,char **argv){
+	struct stat *st;
+	st=(struct stat*)malloc(sizeof(struct stat));
+	stat(argv[1],st);
+	if(S_ISDIR((st->st_mode)& S_IFMT)){
+		printf("file type is: Directory\n");
+	}
+	else if(S_ISCHR((st->st_mode) & S_IFMT )){
+		printf("file type is: Character Device\n");
+	}
+	else if(S_ISBLK((st->st_mode)& S_IFMT)){
+		printf("file type is: Block Device\n");
+	}
+	else if(S_ISREG((st->st_mode)& S_IFMT)){
+		printf("file type is: Regular file\n");
+	}
+	else if(S_ISFIFO((st->st_mode)&S_IFMT)){
+		printf("file type is: FIFO or Pipe\n");
+	}
+	else if(S_ISLNK((st->st_mode)&S_IFMT)){
+		printf("file type is: Symbolic Link\n");
+	}
+	else if(S_ISSOCK((st->st_mode)& S_IFMT)){
+		printf("file type is: Socket\n");
+	}
+	return 0;
+}
---
+++ 15.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main(int argc, char *argv[],char * envp[]){
+	int i;
+	for(int i=0;envp[i]!=NULL;i++){
+		printf("\n%s",envp[i]);
+	}
+}
