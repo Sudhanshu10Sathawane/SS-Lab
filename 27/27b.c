b8cae11c31df1170ae8b8df6c0d91a79096465c7
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-10-16 16:00:43 +0530

    0 & IPC_NOWAIT

---
+++ 27/27b.c
@@ -0,0 +1,19 @@
+#include<unistd.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<stdio.h>
+
+void main() {
+    struct msg {
+        long mtype;
+        char mtext[100];
+    } mq;
+    key_t key = ftok(".", 's');
+    int msgid = msgget(key, IPC_CREAT | 0600);
+
+    printf("Enter msg type: ");
+    scanf("%ld", &mq.mtype);
+
+    msgrcv(msgid, &mq, sizeof(mq.mtext), mq.mtype, IPC_NOWAIT);
+    printf("Message : %s\n", mq.mtext);
+}
