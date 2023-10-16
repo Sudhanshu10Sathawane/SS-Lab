af149014769dabf4c92ab14b8c955824649c4a6e
sudhanshu10sathawane <sudhanshusathwane@gmail.com>
2023-10-16 11:58:51 +0530

    checked adding mssgs

--- 24.c
+++ 24.c
@@ -3,7 +3,7 @@
 #include<sys/msg.h>
 #include<stdio.h>
 
-void main(void) {
+void main() {
     key_t key = ftok(".", 's');
     int msgid = msgget(key, IPC_CREAT | 0666);
     printf("Key: 0x%0x, msg-id: %d\n", key, msgid);   
